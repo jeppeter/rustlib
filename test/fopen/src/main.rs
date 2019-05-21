67800b8bc3c2ed1f74cec93857f6704a9e34fff1
jeppeter <jeppeter@gmail.com>
2019-05-21 15:12:36 +0800

    to pass ok fopen

--- test/fopen/src/main.rs
+++ test/fopen/src/main.rs
@@ -1,16 +1,26 @@
+use std::error::Error;
 use std::fs::File;
+//use std::io::prelude::*;
 use std::env;
 use std::io::BufReader;
 use std::io::BufRead;
 fn main() {
+	let mut i :i32=0;
 	for arg in env::args() {
-		read_file(&arg[..]);
+		if i > 0 {
+			read_file(&arg[..]);	
+		}
+		i += 1;		
 	}
 	return;
 }
 
 fn read_file(f :&str) {
-	let fin = File::open(f).unwrap();
+	let fin =  match File::open(f) {
+		Err(why) => panic!("couldn't open {}: {}", f,
+                                                   why.description()),
+		Ok(fin) => fin,
+	};
 	let  fbuf = BufReader::new(&fin);
 	for (n,line) in fbuf.lines().enumerate() {
 		let l = line.unwrap();
