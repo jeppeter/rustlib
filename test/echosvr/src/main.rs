dd73ebdd63fe355a1aeda4d41b552b72a2644922
jeppeter <jeppeter@gmail.com>
2019-05-23 16:37:19 +0800

    to add echosvr

--- test/echosvr/src/main.rs
+++ test/echosvr/src/main.rs
@@ -1,3 +1,53 @@
+use std::net::{TcpListener,TcpStream};
+use std::thread;
+use std::io::{self,Write,Read};
+use std::env;
+use std::vec::Vec;
+use std::fmt;
+
+fn handle_client(mut stream: TcpStream) {
+	loop {
+		let mut readbuf = [0;1600];
+		match stream.read(&mut readbuf) {
+			Ok(n) => {
+				if n == 0 {
+					break;
+				}
+				stream.write(&readbuf[0..n]).unwrap();
+			},
+			Err(err) => {
+				panic!(err);
+			}
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let argv :Vec<String> = env::args().collect();
+	let mut port:i32 = 3102;
+	let mut bindstr :String;
+	if argv.len() > 1 && 
+		(argv[1] == "-h" ||
+			argv[1] == "--help") {
+			io::stderr().write(b"{} port\n")
+		}
+
+	if argv.len() > 1 {
+		port = argv[1].parse.unwrap_or(3102);
+	}
+	bindstr = format!("127.0.0.1:{}", port);
+	let listener = TcpListener.bind(bindstr);
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				thread::spawn(move || {
+					handle_client(stream)
+				});
+			},
+			Err(err) => {
+				println!("can not accept {} error [{}]", bindstr, err);
+			}
+		}
+	}
+	return;
 }
