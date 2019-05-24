8b621709653a56529db2b82b84c5193415756a1a
jeppeter <jeppeter@gmail.com>
2019-05-24 16:13:50 +0800

    to compile echocli

--- test/echocli/src/main.rs
+++ test/echocli/src/main.rs
@@ -1,3 +1,37 @@
+use std::net::{TcpStream};
+use std::io::{Write,Read};
+use std::env;
+use std::process;
+use std::vec::Vec;
+
 fn main() {
-    println!("Hello, world!");
+	let argv :Vec<String> = env::args().collect();
+	let mut port:i32 = 3102;
+	let mut hoststr:String = "127.0.0.1";
+	let  bindstr:String;
+
+	if argv.len() == 2 && 
+		(argv[1] == "-h" || argv[1] == "--help") {
+		println!("{} host port", argv[0]);
+		process::exit(3);
+	}
+
+	if argv.len() > 1 {
+		hoststr = argv[1];	
+	}
+	
+	if argv.len() > 2 {
+		port = argv[2].parse().unwrap_or(3102);
+	}
+	
+	bindstr = format!("{}:{}", hoststr ,port);
+
+	match TcpStream::connect(bindstr) {
+		Ok(mut stream) => {
+			
+		}
+	}
+
+
+	return;    
 }
