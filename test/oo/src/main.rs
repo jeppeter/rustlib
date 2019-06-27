0812e5a0ff76432ec69939bccee376845190fef6
jeppeter <jeppeter@gmail.com>
2019-06-27 17:25:35 +0800

    to pass oo

--- test/oo/src/main.rs
+++ test/oo/src/main.rs
@@ -1,3 +1,23 @@
+
+struct Rectangle {
+	width :u32,
+	height :u32,
+}
+
+
+impl Rectangle {
+	// add code here
+	fn new(h :u32, w :u32) -> Rectangle {
+		return Rectangle { width:w, height : h};
+	}
+
+	fn area(&self) -> u32 {
+		return self.width * self.height;
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let r :Rectangle;
+	r = Rectangle::new(32,50);
+	println!("area {}", r.area());
 }
