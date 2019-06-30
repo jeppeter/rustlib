10ab1c85aff1fc207f68ed3ca77924521b2b87ce
jeppeter <jeppeter@gmail.com>
2019-06-30 14:20:14 +0800

    to pass fabanaci ok

--- test/fabinaci/src/main.rs
+++ test/fabinaci/src/main.rs
@@ -2,18 +2,19 @@ use std::env;
 use std::vec::Vec;
 
 fn main() {
-	let mut c :i32;
-	let mut v :Vec<i32> = Vec::new();
-	let mut cv =  &v;
+	let mut c :i64;
+	let mut v :Vec<i64> = Vec::new();
 	for arg in env::args() {
 		c = arg.parse().unwrap_or(0);
-		println!("[{}]=[{}]", arg, fabonaci(c,cv));
+		println!("[{}]=[{}]", arg, fabonaci(c,&mut v));
 	}
 }
 
 
-fn fabonaci(i: i32, c :&mut Vec<i32>) -> i32 {
+fn fabonaci<'a>(i: i64, c :&'a mut Vec<i64>) -> i64 {
 	let msize :usize ;
+	let cc1 :i64;
+	let cc2 :i64;
 	msize = i as usize;
 	if  i > 2 {
 		if c.len() < (msize - 1) {
@@ -23,7 +24,9 @@ fn fabonaci(i: i32, c :&mut Vec<i32>) -> i32 {
 			fabonaci(i-2,c);
 		}
 		if c.len() < msize {
-			c.push(c[msize-3] + c[msize-2]);
+			cc1 = c[msize-3];
+			cc2 = c[msize-2];
+			c.push(cc1 + cc2);
 		}
 		return c[msize-1];
 	}  else if i == 2 {
