9cd60e7b5cfcae818c15dbb970eb4b8c1d1f4a34
jeppeter <jeppeter@gmail.com>
2019-10-19 19:18:48 +0800

    to find the error on regtest

--- test/regtest/src/main.rs
+++ test/regtest/src/main.rs
@@ -1,3 +1,17 @@
+use regex::Regex;
+
+
 fn main() {
-    println!("Hello, world!");
+	let re = Regex::new(r"(?x)
+	(?P\d{4})
+	(?P\d{2})
+
+	(?P\d{2})
+	").unwrap();
+	let caps = re.captures("2010-03-14").unwrap();
+
+	assert_eq!("2010", &caps["year"]);
+	assert_eq!("03", &caps["month"]);
+	assert_eq!("14", &caps["day"]);
+
 }
