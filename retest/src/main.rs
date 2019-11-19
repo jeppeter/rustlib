2e8a263369b53c5a6e179adf8098e4a79374b661
jeppeter <jeppeter@gmail.com>
2019-11-19 16:54:42 +0800

    to add match function

--- retest/src/main.rs
+++ retest/src/main.rs
@@ -9,12 +9,25 @@ fn capture_regex(restr :&str, instr :&str) -> bool {
 	}
 }
 
+fn match_regex(restr :&str, instr :&str) -> bool {
+	let re = Regex::new(restr).unwrap();
+	let bmatch = re.is_match(instr);
+	if bmatch {
+		println!("{} match {}", instr, restr);
+		return true;
+	}
+	println!("{} not match {}", instr, restr );
+	return false;
+}
+
 fn usage(ec :i32,_fmtstr :String) {
 	let mut outstr :String = String::from("");
 	if _fmtstr.len() > 0 {
 		outstr.push_str(&(_fmtstr[..]));
 	}
 	outstr.push_str(&(format!("retest [SUBCOMMANDS]\n[SUBCOMMANDS]\n")[..]));
+	outstr.push_str(&(format!("\tcapture restr instr...              to find all matches\n")[..]));
+	outstr.push_str(&(format!("\tmatch   restr instr...              to match string\n")[..]));
 	if ec == 0 {
 		print!("{}",outstr);
 	} else {
@@ -41,7 +54,16 @@ fn main() {
 			capture_regex(&(argv[2][..]),&(argv[i][..]));
 			i = i + 1;
 		}
-	} else {
+	} else if argv[1] == "match"{
+		if argv.len() < 4 {
+			usage(3, format!("match need 4 args"));
+		}
+		i = 3;
+		while i < argv.len() {
+			match_regex(&(argv[2][..]), &(argv[i][..]));
+			i = i + 1;
+		}
+	}else {
 		usage(3,format!("not support {}", argv[1]));
 	}
 	return
