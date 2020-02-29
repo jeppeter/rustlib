f1f68e6bcb17203f29db684ee20bf08850347090
jeppeter <jeppeter@gmail.com>
2020-02-29 20:15:36 +0800

    to  make mutex  lock

--- test/thrx/src/main.rs
+++ test/thrx/src/main.rs
@@ -1,18 +1,24 @@
-use std::sync::{Arc, Barrier};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
 fn  main()  {
 	let mut handles = Vec::with_capacity(10);
-	let barrier = Arc::new(Barrier::new(10));
+	let lck = Arc::new(Mutex::new(0));
 	for i in 0..10 {
-	    let c = barrier.clone();
+	    let cl =  lck.clone();
 	    handles.push(thread::spawn(move|| {
 	        // do some work
-	        println!("i [{}]", i);
-	        thread::sleep(time::Duration::from_millis(100  * (i+1)));
-	        //c.wait();
-	        println!("after i [{}]", i);
-	        c.wait();
+	        match cl.lock()  {
+				Ok(guard)  =>  {
+		        println!("i [{}]", i);
+		        thread::sleep(time::Duration::from_millis(100 ));
+		        //c.wait();
+		        println!("after i [{}]", i);
+		        drop(guard);
+				},
+				Err(poisoned)  =>  {eprintln!("error  {:?}", poisoned);;  return;},
+	        };
+	        //cl.unlock();
 	    }));
 	}
 	// Wait for other threads to finish.
