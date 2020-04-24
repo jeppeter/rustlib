9053cf45353b9aa68746bb6f1f8cb433a2c4b519
jeppeter <jeppeter@gmail.com>
2020-04-24 15:59:56 +0800

    to add callback into repo

---
+++ test/callback/src/main.rs
@@ -0,0 +1,27 @@
+type Callback = fn();
+
+struct Processor {
+    callback: Callback,
+}
+
+impl Processor {
+    fn set_callback(&mut self, c: Callback) {
+        self.callback = c;
+    }
+
+    fn process_events(&self) {
+        (self.callback)();
+    }
+}
+
+fn simple_callback() {
+    println!("hello world!");
+}
+
+fn main() {
+    let mut p = Processor {
+        callback: simple_callback,
+    };
+    p.set_callback(simple_callback);
+    p.process_events(); // hello world!
+}
