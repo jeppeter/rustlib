bba68fef6f947dc6985577eeb7de30ded7ab6eb3
jeppeter <jeppeter@gmail.com>
2020-10-27 21:22:28 +0800

    any to type

--- test/funccall/src/main.rs
+++ test/funccall/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::any::Any;
 
 fn call_func_1(s :&str) {
 	println!("{} call 1",s);
@@ -10,11 +11,28 @@ fn call_func_2(s :&str) {
 	return;
 }
 
+
+
 fn main() {
-	let mut dispatch :HashMap<_,fn(&str)> = HashMap::new();
-	dispatch.insert("call_func_1",call_func_1);
-	dispatch.insert("call_func_2",call_func_2);
-	dispatch["call_func_1"]("cc");
-	dispatch["call_func_2"]("cc");
+	type FuncString = fn(&str);
+	let mut dispatch :HashMap<_,&dyn Any> = HashMap::new();
+	let mut func :&dyn Any;
+	let s :&dyn Any;
+	let funcall :FuncString;
+	func = &call_func_1;
+	println!("{:?}", func);
+	//println!("{}", &call_func_1);
+	dispatch.insert("call_func_1",func);
+	func = &call_func_2;
+	dispatch.insert("call_func_2",func);
+	println!("{:?}",dispatch["call_func_1"]);
+	s = dispatch["call_func_1"];
+	match s.downcast::<FuncString>() {
+		Some(c) => {println!("down succ");},
+		_ => {eprintln!("not succ");}
+	}
+	//funcall("cc");
+	//dispatch["call_func_1"]("cc");
+	//dispatch["call_func_2"]("cc");
 	return;
 }
