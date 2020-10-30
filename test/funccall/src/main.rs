0916527ef0fa829d7f74ca756ce3c3c3ed5bc471
jeppeter <jeppeter@gmail.com>
2020-10-31 04:44:18 +0800

    to pass funcall ok

--- test/funccall/src/main.rs
+++ test/funccall/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::any::Any;
+//use std::any::Any;
 
 fn call_func_1(s :&str) {
 	println!("{} call 1",s);
@@ -11,25 +11,42 @@ fn call_func_2(s :&str) {
 	return;
 }
 
+fn call_func_base_1() {
+	println!("base 111");
+	return;
+}
 
+type FuncString = fn(&str);
+type FuncBase = fn();
+
+
+pub enum Functype {
+	FuncS(FuncString),
+	FuncB(FuncBase),
+}
 
 fn main() {
-	type FuncString = fn(&str);
-	let mut dispatch :HashMap<_,&dyn Any> = HashMap::new();
-	let mut func :&dyn Any;
-	let s :&dyn Any;
-	let funcall :FuncString;
-	func = &call_func_1;
-	println!("{:?}", func);
+	let mut dispatch :HashMap<_,Functype> = HashMap::new();
+	//let mut funcall :FuncString;
+	//println!("{:?}", func);
 	//println!("{}", &call_func_1);
-	dispatch.insert("call_func_1",func);
-	func = &call_func_2;
-	dispatch.insert("call_func_2",func);
-	println!("{:?}",dispatch["call_func_1"]);
-	s = dispatch["call_func_1"];
-	match s.downcast_ref::<FuncString>() {
-		Some(c) => {println!("down succ");},
+	dispatch.insert("call_func_1",Functype::FuncS(call_func_1));
+	dispatch.insert("call_func_2",Functype::FuncS(call_func_2));
+	dispatch.insert("call_func_base_1",Functype::FuncB(call_func_base_1));
+	//println!("{:?}",dispatch["call_func_1"]);
+	match dispatch["call_func_1"] {
+		Functype::FuncS(func) => {func("cc") ;println!("down succ");},
 		_ => {eprintln!("not succ");}
+	}
+
+	match dispatch["call_func_base_1"] {
+		Functype::FuncB(func) => {func();println!("base call");}
+		_ => {eprintln!("no base");}
+	}
+
+	match dispatch["call_func_2"] {
+		Functype::FuncB(func) => {func();println!("call base");}
+		_ => {eprintln!("no func 2");}
 	}
 	//funcall("cc");
 	//dispatch["call_func_1"]("cc");
