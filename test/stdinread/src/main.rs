40048cc35280be1539037926c49639f9c96fbb76
jeppeter <jeppeter@gmail.com>
2020-11-16 17:13:21 +0800

    to make stdinread ok

--- test/stdinread/src/main.rs
+++ test/stdinread/src/main.rs
@@ -2,21 +2,14 @@ use std::io;
 
 fn main() {
     let mut line :String;
-    let mut newline :String;
-    let mut tobytes :&[u8];
-    let mut lastn :i32;
-    let mut curn :i32;
     line = String::new();
-    lastn = 0;
     loop {
 
     	match io::stdin().read_line(&mut line) {
     		Ok(n) => {
-    			curn = n as i32;
-    			tobytes = line.as_bytes();
-    			newline = String::from(tobytes[lastn..curn]);
-    			println!("{:?}", newline);},
-    			lastn = curn;
+    			print!("{}", line);
+    			line = String::new();
+    		},
     		Err(e) => {eprintln!("{:?}", e);}
     	}
     }
