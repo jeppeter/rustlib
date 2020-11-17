289babdd89fb56c628cb09aa41732f1b27486ea5
jeppeter <jeppeter@gmail.com>
2020-11-17 15:17:17 +0800

    to make read file

--- test/stdinread/src/main.rs
+++ test/stdinread/src/main.rs
@@ -1,16 +1,17 @@
-use std::io;
+use std::env;
+use std::fs;
+use std::io::{self, BufReader, BufRead};
 
 fn main() {
-    let mut line :String;
-    line = String::new();
-    loop {
-
-    	match io::stdin().read_line(&mut line) {
-    		Ok(n) => {
-    			print!("{}", line);
-    			line = String::new();
-    		},
-    		Err(e) => {eprintln!("{:?}", e);}
+    let input = env::args().nth(1);
+    let reader: Box<dyn BufRead> = match input {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap()))
+    };
+    for line in reader.lines() {
+    	match line {
+    		Ok(l) => {println!("{}",l );},
+    		Err(_) => {}
     	}
     }
 }
