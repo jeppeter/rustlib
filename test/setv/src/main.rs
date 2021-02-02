e2037918659ee77f057334030fe43174c8f5e6af
jeppeter <jeppeter@gmail.com>
2021-02-02 16:19:04 +0800

    to make as_bytes in rust

--- test/setv/src/main.rs
+++ test/setv/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn modify(v :& mut Vec<i32>) {
 	println!("{:p}", v);
 	v.push(42);
@@ -6,7 +8,19 @@ fn modify(v :& mut Vec<i32>) {
 
 fn main() {
     let mut v = vec![1,2,3];
+    let args :Vec<String> = env::args().collect();
+    let mut bs :&[u8];
+    //let mut i :i32;
     modify(&mut v);
     println!("{:?}", v);
+
+    if args.len() > 1 {
+    	for j in 1..args.len() {
+	    	bs = args[j].as_bytes();
+	    	for i in 0..bs.len() {
+	    		println!("[{}][{}]=[{:x}]",j,i,bs[i]);
+	    	}    		
+    	}
+    }
     return;
 }
