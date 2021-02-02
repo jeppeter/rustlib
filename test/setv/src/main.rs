d0c0fb4d97e684c4bb495f78c6229614609f25eb
jeppeter <jeppeter@gmail.com>
2021-02-02 16:36:38 +0800

    to make the string drop

--- test/setv/src/main.rs
+++ test/setv/src/main.rs
@@ -1,5 +1,16 @@
 use std::env;
 
+#[derive(Debug)]
+struct DeString {
+	s :String
+}
+
+impl Drop for DeString {
+	fn drop(&mut self) {
+		println!("drop {:?} [{:p}]", self.s,self);
+	}
+}
+
 fn modify(v :& mut Vec<i32>) {
 	println!("{:p}", v);
 	v.push(42);
@@ -16,10 +27,12 @@ fn main() {
 
     if args.len() > 1 {
     	for j in 1..args.len() {
+		    let mut s = DeString { s : String::from("c")};
 	    	bs = args[j].as_bytes();
 	    	for i in 0..bs.len() {
 	    		println!("[{}][{}]=[{:x}]",j,i,bs[i]);
 	    	}    		
+	    	s.s = String::from(&(args[j]));
     	}
     }
     return;
