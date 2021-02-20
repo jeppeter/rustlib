5d6e7e768ccbcdfb5dbbfed08a0969273091a140
jeppeter <jeppeter@gmail.com>
2021-02-20 14:26:06 +0800

    to add access

---
+++ test/mod/access/src/main.rs
@@ -0,0 +1,23 @@
+mod outermost {
+	pub fn middle_function() {}
+	fn middle_secret_function() {
+
+	}
+
+	pub mod inside {
+		pub fn inner_function() {}
+		fn secret_function() {}
+	}
+}
+
+fn try_me() {
+	outermost::middle_function();
+	//outermost::middle_secret_function();
+	outermost::inside::inner_function();
+	//outermost::inside::secret_function();
+}
+
+
+fn main() {
+    try_me();
+}
