f8502e242eb122308601f06695716a33a499b3d7
jeppeter <jeppeter@gmail.com>
2021-03-27 12:55:33 +0800

    to make the wait and set event

--- test/win32/wevt/src/main.rs
+++ test/win32/wevt/src/main.rs
@@ -1,15 +1,19 @@
-use winapi::um::synchapi::{CreateEventW};
+use winapi::um::synchapi::{CreateEventW,OpenEventW,WaitForSingleObject,SetEvent};
 use std::env::args;
 use std::os::windows::io::{AsRawHandle, FromRawHandle, IntoRawHandle};
 use winapi::shared::ntdef::{HANDLE};
 use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
+use winapi::um::winnt::{SYNCHRONIZE};
+use winapi::um::winbase::{INFINITE};
 use std::{ptr,fmt};
 use std::error::{Error};
-use winapi::shared::minwindef::{FALSE};
+use winapi::shared::minwindef::{FALSE,DWORD,TRUE};
 use std::ffi::{OsStr};
 use std::os::windows::ffi::{OsStrExt};
 //use lpwstr::ToWide;
 
+
+const NULL_HANDLE :HANDLE = 0x0 as HANDLE;
 
 #[derive(Debug)]
 struct EventError {
@@ -62,9 +66,8 @@ impl IntoRawHandle for TokenHandle {
 
 impl Drop for TokenHandle {
 	fn drop(&mut self) {
-		let nullhdl :HANDLE = 0x0 as HANDLE;
 
-		if self.value != INVALID_HANDLE_VALUE && self.value != nullhdl {
+		if self.value != INVALID_HANDLE_VALUE && self.value != NULL_HANDLE {
 			unsafe { CloseHandle(self.value); }
 		}
 		self.value = INVALID_HANDLE_VALUE;
@@ -79,29 +82,68 @@ fn to_wide(s :&str) -> Vec<u16>  {
 	return OsStr::new(s).encode_wide().chain(Some(0)).collect();
 }
 
+fn open_event(evtname :&str) ->Result<TokenHandle,EventError> {
+	let hdl :HANDLE;
+
+	unsafe {
+		hdl = OpenEventW(SYNCHRONIZE, FALSE, to_wide(evtname).as_ptr());	
+		if hdl == INVALID_HANDLE_VALUE || hdl == NULL_HANDLE {
+			return Err(EventError::new(&(format!("create event [{}] error",evtname)[..])));
+		} 
+		return Ok(TokenHandle::from_raw_handle(hdl));
+	}
+}
+
 
 fn create_event(evtname :&str) -> Result<TokenHandle,EventError> {
 	let hdl :HANDLE;
-	let nullhdl :HANDLE = (0x0 as HANDLE);
 
 	unsafe {
 		hdl = CreateEventW(ptr::null_mut(), FALSE, FALSE, to_wide(evtname).as_ptr());	
 		//hdl = CreateEventW(ptr::null_mut(), FALSE, FALSE, ptr::null());	
-		if hdl == INVALID_HANDLE_VALUE || hdl == nullhdl {
+		if hdl == INVALID_HANDLE_VALUE || hdl == NULL_HANDLE {
 			return Err(EventError::new(&(format!("create event [{}] error",evtname)[..])));
 		} 
-		println!("create {:?}", hdl);
 		return Ok(TokenHandle::from_raw_handle(hdl));
 	}	
 }
 
+fn wait_event(evt :&TokenHandle, _timeout :i32) -> bool {
+	let dret :DWORD;
+	unsafe {
+		dret = WaitForSingleObject(evt.as_raw_handle(),INFINITE);
+		println!("dret {:?}", dret);
+		if dret == 0 {
+			return true;
+		}
+	}
+	return false;
+}
+
+fn set_event(evt :&TokenHandle) -> bool {
+	unsafe {
+		SetEvent(evt.as_raw_handle());
+	}
+	return true;
+}
+
+
 fn main() {
 	let argv:Vec<String> = args().collect();
 	let evt :TokenHandle;
+	let _bret :bool;
 	if argv.len() > 1 {
-		if argv[1] == "create" {
+		if argv[1] == "wait" {
 			evt = create_event(&(argv[2])).unwrap();
 			println!("create [{}] succ [{:?}]", argv[2],evt);
+			//set_event(&evt);
+			_bret = wait_event(&evt,300);
+			println!("wait [{}] event",argv[2]);
+		} else if argv[1] == "setevent" {
+			evt = open_event(&(argv[2])).unwrap();
+			println!("open [{}] succ [{:?}]", argv[2],evt);
+			set_event(&evt);
+			println!("set event [{}]",argv[2]);
 		} else {
 			eprintln!("{} not support command",argv[1] );
 
