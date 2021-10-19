5b91c1b7446eb0372cc9e06116f91a64434b5369
jeppeter <jeppeter@gmail.com>
2021-10-19 17:28:06 +0800

    not ok for moved data

--- test/thread/sharedata/src/main.rs
+++ test/thread/sharedata/src/main.rs
@@ -39,14 +39,68 @@ impl Writers {
 	}
 }
 
+pub struct Sharedata {
+	pub exit : u8,
+	pub thr : Option<JoinHandle<()>>,
+}
+
+impl Sharedata {
+	pub fn new() -> Sharedata {
+		Sharedata{ exit : 0, thr: None}
+	}
+	pub fn try_join(&mut self) -> bool {
+			if self.exit == 1 {
+				match self.thr {
+					Some(v2) => {
+						self.thr = None;
+						v2.join().unwrap();
+						return true;
+					},
+					None => {}
+				}
+			}
+			return false;
+	}
+
+	pub fn join(&mut self) -> bool {
+		if self.exit == 1 {
+			match self.thr {
+				Some(v2) => {
+					self.thr = None;
+					v2.join().unwrap();
+					return true;
+				},
+				None => {
+
+				}
+			}
+		} else {
+			match self.thr {
+				Some(v2) => {
+					self.thr = None;
+					v2.join().unwrap();
+					self.exit = 1;
+					return true;
+				},
+				None => {}
+			}
+		}
+		return false;
+	}
+}
+
 
 fn main() {
 
 	let  w :Arc<Mutex<Writers>> = Arc::new(Mutex::new(Writers::new()));
-	let mut thrvec :Vec<Box<JoinHandle<_>>> =  Vec::new();
+	//let mut thrvec :Vec<Arc<JoinHandle<_>>> =  Vec::new();
+	let mut shdata :Vec<Arc<Mutex<Sharedata>>> = Vec::new();
 
-	for i in 1..10 {
+	for i in 0..10 {
 		let cw = w.clone();
+		let sw = Arc::new(Mutex::new(Sharedata::new()));
+		let sd = sw.clone();
+
 		let thr = thread::spawn(move || {
 			{
 				let mut cb = cw.lock().unwrap();
@@ -54,16 +108,43 @@ fn main() {
 				println!("thread {} {}",i,cb.description());
 			}
 			thread::sleep(time::Duration::from_millis(1000));
+			{
+				let mut cb = sd.lock().unwrap();
+				cb.exit = 1;
+			}
 		});
-		thrvec.push(Box::new(thr));
+		{
+			let mut cb = sw.lock().unwrap();
+			cb.thr = Some(thr);
+		}
+		shdata.push(sw);
 	}
 
-	for v in thrvec {
-		v.join().unwrap();
+	for i in 0..shdata.len() {
+		let v = shdata[i].clone();
+		{
+			let mut cb = v.lock().unwrap();
+			if cb.exit == 1 {
+				println!("[{}] exited", i);
+				cb.try_join();
+			} else {
+				println!("[{}] not exited", i);
+			}
+		}
+		thread::sleep(time::Duration::from_millis(500));
+	}
+
+	for i in 0..shdata.len() {
+		let v = shdata[i].clone();
+		{
+			let mut cb = v.lock().unwrap();
+			cb.join();
+		}
 	}
 	{
 		let cb = w.lock().unwrap();
 		println!("descrip {}",  cb.description());
 	}
+
 	return;
 }
