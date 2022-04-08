15f2f573a113b1c5293f801252a42642ef0317e5
jeppeter <jeppeter@gmail.com>
2022-04-08 14:08:00 +0800

    to make boxn with box ok

--- test/boxn/src/main.rs
+++ test/boxn/src/main.rs
@@ -1,15 +1,30 @@
 
+
+
+#[allow(dead_code)]
 #[derive(Debug)]
 struct PoinX {
 	x :f64,
-	y :f64
+	y :f64,
+	next :Option<Box<PoinX>>
 }
 
 fn new_poinx(x1:f64,y1:f64) -> PoinX{
-	PoinX{x:x1,y:y1}
+	PoinX{x:x1,y:y1,next:None}
+}
+
+impl PoinX {
+	fn add_next(&mut self,v :Option<Box<PoinX>>) -> &PoinX{
+		self.next = v;
+		self
+	}
 }
 
 fn main() {
-    let xc :Box<PoinX> = Box::new(new_poinx(2.1,32.21));
+    let mut xc :Box<PoinX> = Box::new(new_poinx(1.1,1.1));
+    let mut c :Box<PoinX> = Box::new(new_poinx(2.2,2.2));
+    let bc :Box<PoinX> = Box::new(new_poinx(3.3,3.3));
+    c.add_next(Some(bc));
+    xc.add_next(Some(c));
     println!("xc {:?}", xc);
 }
