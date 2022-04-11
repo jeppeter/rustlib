183c492a81a430b7a266fa2120079673d77ba944
jeppeter <jeppeter@gmail.com>
2022-04-11 15:03:43 +0800

    to make run events ok in thread

--- test/thread/evts/src/main.rs
+++ test/thread/evts/src/main.rs
@@ -1,11 +1,49 @@
 
-extern "C" {
-	fn abs(i :i32) -> i32;
-}
+use synchronoise::event::{SignalEvent, SignalKind};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-	unsafe {
-		println!("abs call [{}]",abs(-5));	
+	//let start_signal = Arc::new(SignalEvent::new(false, SignalKind::Manual));
+	//let stop_signal = Arc::new(SignalEvent::new(false, SignalKind::Auto));
+	let thread_count = 5;
+	let mut startvec :Vec<Arc<SignalEvent>> = Vec::new();
+	let mut stopvec :Vec<Arc<SignalEvent>> = Vec::new();
+	let mut uidx :usize;
+
+	for i in 0..thread_count {
+		let curstart = Arc::new(SignalEvent::new(false,SignalKind::Manual));
+		let curstop = Arc::new(SignalEvent::new(false,SignalKind::Auto));
+		let start = curstart.clone();
+		let stop = curstop.clone();
+		startvec.push(curstart);
+		stopvec.push(curstop);
+		thread::spawn(move || {
+        //as a Manual-reset signal, all the threads will start at the same time
+        start.wait();
+        thread::sleep(Duration::from_secs(i));
+        println!("thread {} activated!", i);
+        stop.signal();
+    });
 	}
-    
+
+	uidx=0;
+	while uidx < startvec.len() {
+		startvec[uidx].signal();
+		uidx += 1;
+	}
+
+	while stopvec.len() > 0 {
+		for i in 0..stopvec.len() {
+			if stopvec[i].status(){
+				stopvec.remove(i);
+				println!("remove [{}]",i);
+				break;
+			}
+		}
+	}
+
+
+	println!("all done!");
 }
