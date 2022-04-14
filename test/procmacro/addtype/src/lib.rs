01ad3db431024012156f0075cd96a7d1d04b10c8
jeppeter <jeppeter@gmail.com>
2022-04-14 16:27:32 +0800

    to make types ok

--- test/procmacro/addtype/src/lib.rs
+++ test/procmacro/addtype/src/lib.rs
@@ -1,15 +1,38 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn;
+use std::sync::{Mutex,Arc};
+use lazy_static::lazy_static;
+//use std::cell::RefCell;
+//use std::rc::Rc;
+
+
+lazy_static! {
+	static ref LINK_NAMES :Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+}
+
+//fn get_static_names() -> Rc<RefCell<Vec<String>>> {
 
 #[proc_macro_attribute]
-fn print_func_name(args :TokenStream, input :TokenStream) -> TokenStream {
+pub fn print_func_name(_args :TokenStream, input :TokenStream) -> TokenStream {
+	match syn::parse(input.clone()) {
+		Ok(v1) => {
+			let v :syn::ItemFn = v1;
+			{
+				let c = LINK_NAMES.clone();
+				let mut cb = c.lock().unwrap();
+				cb.push(v.sig.ident.to_string());
+			}
+			
+		},
+		Err(e) => {
+			eprintln!("error {}", e);
+		}
+	}
 	println!("call print_func_name");
+
 	input
 }
 
-#[print_func_name]
-fn hello_world() {
-	println!("hello world");
-}
 
