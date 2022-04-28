4624018b47e60d9bbe0ff91fdd1128d5b2226470
jeppeter <jeppeter@gmail.com>
2022-04-28 16:27:40 +0800

    to make clone function ok

--- test/fncall/src/main.rs
+++ test/fncall/src/main.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+//use std::sync::Mutex;
+use std::cell::RefCell;
+
+
 
 fn add(i :i32, j :i32) -> i32 {
 	println!("add function {}+{}={}", i,j,i+j);
@@ -9,11 +15,77 @@ fn add_2(i :i32, j :i32) -> i32 {
 	return i+j+2;
 }
 
+#[derive(Clone)]
+pub struct VFn {
+	innermap : HashMap<String,Arc<dyn Fn(String) -> i32>>,
+}
+
+
+impl VFn {
+	pub fn new() -> VFn {
+		VFn{
+			innermap : HashMap::new(),
+		}
+	}
+
+	pub fn hello(&mut self,c :String) -> i32 {
+		println!("hello from self");
+		c.len() as i32
+	}
+
+	pub fn world(&mut self,c :String) -> i32 {
+		println!("world from self");
+		c.len() as i32
+	}
+
+	pub fn insertmaps(&mut self) {
+		let b = Arc::new(RefCell::new(self.clone()));
+		let c = b.clone();
+		self.innermap.insert(format!("hello"), Arc::new(move |x| {  c.borrow_mut().hello(x) } ));
+		let e = b.clone();
+		self.innermap.insert(format!("world"),Arc::new(move |x| {  e.borrow_mut().world(x) }));
+	}
+
+	pub fn call_fn(&mut self) {
+		match self.innermap.get_mut("hello") {
+			Some(f) => {
+				let c = f(format!("call"));
+				println!("retval [{}]",c);
+			},
+			None => {println!("None");}
+		}
+
+		match self.innermap.get_mut("world") {
+			Some(f) => {
+				let c = f(format!("call"));
+				println!("world retval [{}]",c);
+			},
+			None => {println!("None");}
+		}
+
+	}
+}
+
+
 fn main() {
 	let fnptr : fn(i32,i32) -> i32 = add;
 	let fn2ptr = add_2;
+	let text = "call text";
+	let mut closures :Vec<Box<dyn Fn()>> = Vec::new();
 	fnptr(3,2);
 	fn2ptr(3,2);
 	(fnptr)(3,10);
 	(fn2ptr)(20,10);
+	closures.push(Box::new(|| println!("first")));
+	closures.push(Box::new(|| println!("second {}",text)));
+	closures.push(Box::new(|| println!("third")));
+	for f in closures.iter() {
+		f();
+	}
+
+	{
+		let mut c = VFn::new();
+		c.insertmaps();
+		c.call_fn();
+	}
 }
