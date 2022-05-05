dc863e4c782866e33e08eca9ce643b3bd4fe327f
jeppeter <jeppeter@gmail.com>
2022-05-05 14:27:37 +0800

    to make out functions

--- test/args/src/main.rs
+++ test/args/src/main.rs
@@ -1,39 +1,58 @@
 use std::env;
 use std::i64;
-use std::fmt::Write;
+use std::io::{Write,BufWriter};
+
+fn write_fmts<T : Write>(iowrite :&mut T,s :&str) -> Result<usize,std::io::Error> {
+	let mut c = iowrite.write(s.as_bytes())?;
+	c += iowrite.write(b"\n")?;
+	Ok(c)
+	//iowrite.write_str("\n")
+}
 
 fn main() {
 	let mut i=0;
 	let mut v :i64;
 	let mut base :u32;
 	let mut cparse :String;
-	let mut wstr :String = String::new();
+	//let mut wstr :String = String::new();
+	//let mut buf = vec![];
+	{
+		//let mut wstr = BufWriter::new(&mut buf);
+		let mut wstr = std::io::stdout();
 
-    for arg in env::args() {
-    	writeln!(wstr,"[{}]=[{}]", i, arg).unwrap();
-    	i += 1;
-    	if i == 1 {
-    		continue;
-    	}
-    	base = 10;
-    	cparse = format!("{}",arg);
-    	if arg.starts_with("0x") || arg.starts_with("0X") {
-    		cparse = cparse[2..].to_string();
-    		base = 16;
-    	}
-    	match i64::from_str_radix(&cparse,base) {
-    		Ok(c) => {
-    			v=  c;
-		    	//println!("[{}]={}",arg,v);
-		    	writeln!(wstr,"[{}]={}",arg,v).unwrap();
+		let mut cstr :String;
 
-    		},
-    		Err(e) => {
-    			//println!("[{}] error[{:?}]", arg,e);
-    			writeln!(wstr,"[{}] error[{:?}]", arg,e).unwrap();
-    		}
-    	}
-    }
+	    for arg in env::args() {
+	    	cstr = format!("[{}]=[{}]", i, arg);
+	    	write_fmts(&mut wstr,&cstr).unwrap();
+	    	//writeln!(wstr,"[{}]=[{}]", i, arg).unwrap();
+	    	i += 1;
+	    	if i == 1 {
+	    		continue;
+	    	}
+	    	base = 10;
+	    	cparse = format!("{}",arg);
+	    	if arg.starts_with("0x") || arg.starts_with("0X") {
+	    		cparse = cparse[2..].to_string();
+	    		base = 16;
+	    	}
+	    	match i64::from_str_radix(&cparse,base) {
+	    		Ok(c) => {
+	    			v=  c;
+			    	//println!("[{}]={}",arg,v);
+			    	cstr = format!("[{}]={}",arg,v);
+			    	write_fmts(&mut wstr, &cstr).unwrap();
+			    	//writeln!(wstr,"[{}]={}",arg,v).unwrap();
 
-    print!("total str\n{}",wstr);
+	    		},
+	    		Err(e) => {
+	    			//println!("[{}] error[{:?}]", arg,e);
+	    			cstr = format!("[{}] error[{:?}]", arg,e);
+	    			write_fmts(&mut wstr,&cstr).unwrap();
+	    		}
+	    	}
+	    }
+	}
+    //let s = std::str::from_utf8(&buf).unwrap();
+    //print!("total str\n{}",s);
 }
