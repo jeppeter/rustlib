6156446c7c01fd4017501de1c35c936aee937e0a
jeppeter <jeppeter@gmail.com>
2022-05-21 18:22:20 +0800

    to make template for extargsparse calling

---
+++ test/callextargs/src/main.rs
@@ -0,0 +1,37 @@
+#[allow(unused_imports)]
+use extargsparse_codegen::{extargs_load_commandline,ArgSet,extargs_map_function};
+#[allow(unused_imports)]
+use extargsparse_worker::{extargs_error_class};
+#[allow(unused_imports)]
+use extargsparse_worker::namespace::{NameSpaceEx};
+#[allow(unused_imports)]
+use extargsparse_worker::argset::{ArgSetImpl};
+use extargsparse_worker::parser::{ExtArgsParser};
+use extargsparse_worker::funccall::{ExtArgsParseFunc};
+
+#[allow(unused_imports)]
+use std::cell::RefCell;
+#[allow(unused_imports)]
+use std::sync::Arc;
+#[allow(unused_imports)]
+use std::error::Error;
+use std::boxed::Box;
+#[allow(unused_imports)]
+use regex::Regex;
+#[allow(unused_imports)]
+use std::any::Any;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+
+#[extargs_map_function()]
+fn main() -> Result<(),Box<dyn Error>> {
+	let loads = r#"{
+		"verbose|v" : "+"
+	}"#;
+	let parser :ExtArgsParser = ExtArgsParser::new(None,None)?;
+	extargs_load_commandline!(parser,loads)?;
+	let ns = parser.parse_commandline(None,None)?;
+	return Ok(());
+}
+
