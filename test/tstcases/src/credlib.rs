e284802081fecab70de296cc468adf6da98ff2dc
jeppeter <jeppeter@gmail.com>
2022-06-15 13:31:37 +0800

    to import winapi functions

--- test/tstcases/src/credlib.rs
+++ test/tstcases/src/credlib.rs
@@ -1,6 +1,11 @@
 #[allow(unused_imports)]
 use extargsparse_worker::{extargs_error_class,extargs_new_error};
 
+#[allow(unused_imports)]
+use winapi::um::wincred::{CredUIPromptForWindowsCredentialsW,CredUnPackAuthenticationBufferW,CREDUI_INFOW,PCREDUI_INFOW};
+#[allow(unused_imports)]
+use winapi::um::combaseapi::{CoTaskMemFree};
+
 use std::error::Error;
 
 #[allow(non_snake_case)]
