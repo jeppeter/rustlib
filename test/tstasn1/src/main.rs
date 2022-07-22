c196beced5ddb054fd94d0f1e3e356b1258dcb2f
jeppeter <jeppeter@gmail.com>
2022-07-23 07:05:56 +0800

    to make tstasn1

---
+++ test/tstasn1/src/main.rs
@@ -0,0 +1,11 @@
+use asn1obj_codegen::{asn1_choice};
+use asn1obj::base::{Asn1Integer,Asn1Object};
+use asn1obj::asn1impl::{Asn1Op};
+
+#[asn1_choice()]
+struct Asn1B
+
+
+fn main() {
+    println!("Hello, world!");
+}
