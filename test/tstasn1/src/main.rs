2cf5e0aefbf56545db2cebf6c1429724a5ef5f21
jeppeter <jeppeter@gmail.com>
2022-07-23 10:30:52 +0800

    to add tstasn1

--- test/tstasn1/src/main.rs
+++ test/tstasn1/src/main.rs
@@ -1,11 +1,23 @@
-use asn1obj_codegen::{asn1_choice};
-use asn1obj::base::{Asn1Integer,Asn1Object};
-use asn1obj::asn1impl::{Asn1Op};
+use asn1obj_codegen::{asn1_choice,asn1_selector};
+use asn1obj::base::{Asn1Integer,Asn1Object,Asn1String};
+use asn1obj::asn1impl::{Asn1Op,Asn1Selector};
+
+
+#[asn1_selector(1.2.3=ci,1.2.5=co,1.2.4=cs)]
+struct Asn1ObjSelector {
+	pub val :Asn1Object,
+}
+
 
 #[asn1_choice()]
-struct Asn1B
+struct Asn1BB {
+	pub selector : Asn1ObjSelector,
+	pub ci :Asn1Integer,
+	pub co :Asn1Object,
+	pub cs :Asn1String,
+}
 
 
 fn main() {
-    println!("Hello, world!");
+	println!("hello world");
 }
