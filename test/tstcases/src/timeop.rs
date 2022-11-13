6f6db26145a1d8e44149e962f4243993b35438a9
jeppeter <jeppeter@gmail.com>
2022-11-13 18:36:59 +0800

    to make timeop unwrap with opt

--- test/tstcases/src/timeop.rs
+++ test/tstcases/src/timeop.rs
@@ -3,7 +3,8 @@ use chrono::prelude::*;
 use std::error::Error;
 
 pub fn timesec_to_tm(tsec :u64) -> Result<String,Box<dyn Error>> {
-	let n : NaiveDateTime=  NaiveDateTime::from_timestamp(tsec as i64, 0);	
+	//let n : NaiveDateTime=  NaiveDateTime::from_timestamp(tsec as i64, 0);	
+	let n : NaiveDateTime=  NaiveDateTime::from_timestamp_opt(tsec as i64, 0).unwrap();	
 	let dt :DateTime<Utc> = DateTime::from_utc(n,Utc);
 	let s = format!("{}",dt.format("%Y-%m-%d %H:%M:%S"));
 	Ok(s)
