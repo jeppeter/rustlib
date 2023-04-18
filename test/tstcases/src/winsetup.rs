dcbfe8f75e93e05fe1bed787b03f99b1d3ef18df
jeppeter <jeppeter@gmail.com>
2023-04-18 11:25:45 +0800

    to add allocate functions

--- test/tstcases/src/winsetup.rs
+++ test/tstcases/src/winsetup.rs
@@ -28,125 +28,157 @@ use super::{debug_trace,debug_buffer_trace,format_buffer_log,format_str_log};
 use super::loglib::{log_get_timestamp,log_output_function,init_log};
 
 use winapi::shared::guiddef::{GUID};
+//use winapi::um::setupapi::{SetupDiGetClassDevsW,SetupDiDestroyDeviceInfoList,HDEVINFO,DIGCF_ALLCLASSES,DIGCF_DEVICEINTERFACE,DIGCF_PRESENT};
+use winapi::um::setupapi::*;
+use winapi::um::handleapi::{INVALID_HANDLE_VALUE};
+use winapi::shared::minwindef::{DWORD,BOOL};
+use std::ptr::null_mut;
 
 
 extargs_error_class!{WinSetupError}
 
 
 struct HwProp {
-	guid :String,
-	propidx :u32,
-	buf :Vec<u8>,
+    guid :String,
+    propidx :u32,
+    buf :Vec<u8>,
 }
 
 struct HwInfo {
-	pros :Vec<HwProp>,
+    pros :Vec<HwProp>,
 }
 
 macro_rules! GET_HEX_VAL {
-	( $tv:expr, $ch :expr) => {
-		$tv <<= 4;
-		if $ch >= '0' as u8 && $ch <= '9' as u8 {
-			$tv += ($ch - ('0' as u8)) as u32;
-		} else if $ch >= 'a' as u8 && $ch <= 'f' as u8 {
-			$tv += ($ch - ('a' as u8) + 10) as u32;
-		} else if $ch >= 'A' as u8 && $ch <= 'F' as u8 {
-			$tv += ($ch - ('A' as u8) + 10) as u32;
-		} else {
-			extargs_new_error!{WinSetupError,"not valid char [{}]", $ch}
-		}
-	}
+    ( $tv:expr, $ch :expr) => {
+        $tv <<= 4;
+        if $ch >= '0' as u8 && $ch <= '9' as u8 {
+            $tv += ($ch - ('0' as u8)) as u32;
+        } else if $ch >= 'a' as u8 && $ch <= 'f' as u8 {
+            $tv += ($ch - ('a' as u8) + 10) as u32;
+        } else if $ch >= 'A' as u8 && $ch <= 'F' as u8 {
+            $tv += ($ch - ('A' as u8) + 10) as u32;
+        } else {
+            extargs_new_error!{WinSetupError,"not valid char [{}]", $ch}
+        }
+    }
 }
 
 fn parse_guid(ins :&str) -> Result<GUID,Box<dyn Error>> {
-	let mut retv :GUID = GUID::default();
-	let bs = ins.as_bytes();
-	let mut cv :u32;
-	if bs[0] != ('{' as u8) {
-		extargs_new_error!{WinSetupError,"bs[0] [{}] != {{", bs[0] as char}
-	}
+    let mut retv :GUID = GUID::default();
+    let bs = ins.as_bytes();
+    let mut cv :u32;
+    if bs[0] != ('{' as u8) {
+        extargs_new_error!{WinSetupError,"bs[0] [{}] != {{", bs[0] as char}
+    }
 
-	cv = 0;
-	for i in 1..9 {
-		GET_HEX_VAL!(cv,bs[i]);
-	}
+    cv = 0;
+    for i in 1..9 {
+        GET_HEX_VAL!(cv,bs[i]);
+    }
 
-	retv.Data1 = cv;
-	if bs[9] != '-' as u8 {
-		extargs_new_error!{WinSetupError,"bs[9] [{}] != -",bs[9] as char}
-	}
+    retv.Data1 = cv;
+    if bs[9] != '-' as u8 {
+        extargs_new_error!{WinSetupError,"bs[9] [{}] != -",bs[9] as char}
+    }
 
-	cv = 0;
-	for i in 10..14 {
-		GET_HEX_VAL!(cv,bs[i]);
-	}
+    cv = 0;
+    for i in 10..14 {
+        GET_HEX_VAL!(cv,bs[i]);
+    }
 
-	retv.Data2 = cv as u16;
+    retv.Data2 = cv as u16;
 
-	if bs[14] != '-' as u8 {
-		extargs_new_error!{WinSetupError,"bs[14] [{}] != -",bs[14] as char}
-	}
+    if bs[14] != '-' as u8 {
+        extargs_new_error!{WinSetupError,"bs[14] [{}] != -",bs[14] as char}
+    }
 
-	cv = 0;
-	for i in 15..19 {
-		GET_HEX_VAL!(cv,bs[i]);
-	}
+    cv = 0;
+    for i in 15..19 {
+        GET_HEX_VAL!(cv,bs[i]);
+    }
 
-	retv.Data3 = cv as u16;
+    retv.Data3 = cv as u16;
 
-	if bs[19] != '-' as u8 {
-		extargs_new_error!{WinSetupError,"bs[19] [{}] != -",bs[19] as char}
-	}
+    if bs[19] != '-' as u8 {
+        extargs_new_error!{WinSetupError,"bs[19] [{}] != -",bs[19] as char}
+    }
 
-	cv = 0;
-	for i in 20..24 {
-		GET_HEX_VAL!(cv,bs[i]);
-		if (i % 2) != 0 {
-			retv.Data4[((i- 20) / 2)] = cv as u8;
-			cv = 0;			
-		}
-	}
+    cv = 0;
+    for i in 20..24 {
+        GET_HEX_VAL!(cv,bs[i]);
+        if (i % 2) != 0 {
+            retv.Data4[((i- 20) / 2)] = cv as u8;
+            cv = 0;         
+        }
+    }
 
-	if bs[24] != '-' as u8 {
-		extargs_new_error!{WinSetupError,"bs[24] [{}] != -",bs[24] as char}
-	}
+    if bs[24] != '-' as u8 {
+        extargs_new_error!{WinSetupError,"bs[24] [{}] != -",bs[24] as char}
+    }
 
-	cv = 0;
-	for i in 25..37 {
-		GET_HEX_VAL!(cv,bs[i]);
-		if (i % 2) == 0 {
-			retv.Data4[(i- 21)/2] = cv as u8;
-			cv = 0;
-		}
-	}
+    cv = 0;
+    for i in 25..37 {
+        GET_HEX_VAL!(cv,bs[i]);
+        if (i % 2) == 0 {
+            retv.Data4[(i- 21)/2] = cv as u8;
+            cv = 0;
+        }
+    }
 
-	if bs[37] != '}' as u8 {
-		extargs_new_error!{WinSetupError,"bs[37] [{}] != }}",bs[37] as char}
-	}
+    if bs[37] != '}' as u8 {
+        extargs_new_error!{WinSetupError,"bs[37] [{}] != }}",bs[37] as char}
+    }
 
-	Ok(retv)
+    Ok(retv)
 }
 
-fn get_hw_infos(guid :* const GUID) -> Result<Vec<HwInfo>,Box<dyn Error>> {
-	let mut retv :Vec<HwInfo> = Vec::new();
+unsafe fn get_hw_props(pinfo :HDEVINFO,pndata :PSP_DEVINFO_DATA) -> Result<Vec<HwProp>,Box<dyn Error>> {
+    let mut bret :BOOL;
+    let mut requiresize :DWORD = 0;
+    let mut props :Vec<HwProp> = Vec::new();
+    let mut propguids :*DEVPROPKEY = null_mut();
 
-	Ok(retv)
+    bret = SetupDiGetDevicePropertyKeys(pinfo,pndata,null_mut(),0,&mut requiresize,0);
+    if bret == 0 {
+        
+    }
+    Ok(props)
+}
+
+fn get_hw_infos(guid :* const GUID,flags :DWORD) -> Result<Vec<HwInfo>,Box<dyn Error>> {
+    unsafe {
+        let mut retv :Vec<HwInfo> = Vec::new();
+        let mut pinfo :HDEVINFO = SetupDiGetClassDevsW(guid,null_mut(),null_mut(),flags);
+
+        if pinfo == INVALID_HANDLE_VALUE{
+            extargs_new_error!{WinSetupError,"can not create DeviceInfoList"}
+        }
+
+        if pinfo != INVALID_HANDLE_VALUE {
+            SetupDiDestroyDeviceInfoList(pinfo);    
+        }   
+        pinfo = INVALID_HANDLE_VALUE;
+
+        Ok(retv)        
+    }
 }
 
 fn lshwinfo_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {  
     let sarr :Vec<String>;
-    let mut ptrguid :* const GUID = std::ptr::null_mut();
+    let mut ptrguid :* const GUID = null_mut();
     let mut guidget :GUID = GUID::default();
+    let mut flags :DWORD = DIGCF_ALLCLASSES;
 
     init_log(ns.clone())?;
     sarr = ns.get_array("subnargs");
 
     if sarr.len() > 0 {
-    	guidget = parse_guid(&sarr[0])?;
-    	ptrguid = &guidget;
+        guidget = parse_guid(&sarr[0])?;
+        flags = DIGCF_PRESENT | DIGCF_DEVICEINTERFACE;
+        ptrguid = &guidget;
     }
     debug_trace!("guid {:?}",guidget);
-    let hwinfos = get_hw_infos(ptrguid)?;
+    let hwinfos = get_hw_infos(ptrguid,flags)?;
 
     Ok(())
 }
@@ -156,9 +188,9 @@ fn lshwinfo_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImp
 pub fn load_ecc_handler(parser :ExtArgsParser) -> Result<(),Box<dyn Error>> {
     let cmdline = r#"
     {
-    	"lshwinfo<lshwinfo_handler>##[guids]... to list handle of guids##" : {
-    		"$" : "*"
-    	}
+        "lshwinfo<lshwinfo_handler>##[guids]... to list handle of guids##" : {
+            "$" : "*"
+        }
     }
     "#;
     extargs_load_commandline!(parser,cmdline)?;
