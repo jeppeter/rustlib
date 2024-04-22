372c42292cfdf77b03c4925f0b04cd46293ba69a
jeppeter <jeppeter@gmail.com>
2024-04-22 10:00:49 +0800

    to make jsontst compile ok

---
+++ test/jsontst/src/jsonlib.rs
@@ -0,0 +1,279 @@
+use extargsparse_codegen::{extargs_load_commandline,extargs_map_function};
+use extargsparse_worker::namespace::{NameSpaceEx};
+use extargsparse_worker::funccall::ExtArgsParseFunc;
+use extargsparse_worker::parser::ExtArgsParser;
+#[allow(unused_imports)]
+use extargsparse_worker::argset::{ArgSetImpl};
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use std::error::Error;
+use serde_json::{json,Value};
+use super::strop::{format_tabs};
+use super::fileop::{read_file};
+use super::logtrans::{init_log};
+
+#[allow(unused_imports)]
+use extargsparse_worker::{extargs_error_class,extargs_new_error};
+
+use std::sync::{Arc};
+use std::cell::{RefCell};
+use std::any::Any;
+
+
+fn array_json(_key :&str, _instr :&str, tabs :i32) -> String {
+	let mut retstr :String = String::from("");
+	let vv :&Vec<Value>;
+	let d :Value;
+	//let mut cnt:usize;
+	match serde_json::from_str(_instr) {
+		Ok(v)  => {
+			d = v;
+			match d.as_array() {
+				Some(dv) => { vv = dv;},
+				None => {eprintln!("---------------\n{}+++++++++++++++++\nnot array",_instr); return retstr;}
+			}
+		}, 
+		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
+	}
+
+	retstr.push_str(&(format_tabs(tabs)[..]));
+	if _key.len() > 0 {
+		retstr.push_str(&(format!("\"{}\" : [",_key)[..]));
+	} else {
+		retstr.push_str("[");
+	}
+
+	for (i,v) in vv.iter().enumerate() {
+		if i > 0 {
+			retstr.push_str(",");
+		}
+		retstr.push_str("\n");
+		if v.is_object() {
+			retstr.push_str(&(enumerate_json("",&(v.to_string()[..]), tabs + 1)[..]));
+			continue;
+		}
+		if v.is_array() {
+			retstr.push_str(&(array_json("",&(v.to_string()[..]),tabs + 1)[..]));
+			continue;
+		}
+
+		retstr.push_str(&(format_tabs(tabs + 1)[..]));
+		retstr.push_str(&(format!("{}",v.to_string())[..]));
+	}
+
+	retstr.push_str("\n");
+	retstr.push_str(&(format_tabs(tabs)[..]));
+	retstr.push_str(&(format!("]")[..]));
+	return retstr;
+}
+
+
+fn enumerate_json(_key :&str,_instr :&str,tabs :i32) -> String {
+	let d :HashMap<String,Value>;
+	let mut retstr :String = String::from("");
+	let mut i:i32;
+	match serde_json::from_str(_instr) {
+		Ok(v) => {d = v;},
+		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
+	}
+
+	retstr.push_str(&(format_tabs(tabs)[..]));
+	if _key.len() > 0 {
+		retstr.push_str(&(format!("\"{}\" : {{",_key)[..]));		
+	} else {
+		retstr.push_str(&(format!("{{")[..]));
+	}
+
+
+	i = 0;
+	for (s,v) in d {
+		if i > 0 {
+			retstr.push_str(",");
+		}
+		retstr.push_str("\n");
+		if v.is_object() {
+			retstr.push_str(&(enumerate_json(&(s[..]),&(v.to_string()[..]),tabs + 1)[..]));
+			i += 1;
+			continue;
+		}
+
+		if v.is_array() {
+			retstr.push_str(&(array_json(&(s[..]),&(v.to_string()[..]),tabs+1)[..]));
+			i += 1;
+			continue;
+		}
+
+		retstr.push_str(&(format_tabs(tabs+1)[..]));
+		retstr.push_str(&(format!("\"{}\" : {}",s,v)[..]));
+		i += 1;
+	}
+	if i > 0 {
+		retstr.push_str("\n");
+	}
+	retstr.push_str(&(format_tabs(tabs)[..]));
+	retstr.push_str(&(format!("}}")[..]));
+	return retstr;
+}
+
+
+fn add_json_value(_instr :&str, _key :&str, _val:&str) -> String {
+	let mut d :HashMap<String,Value>;
+	let c :Value;
+	let mut retstr :String = String::from("");
+	match serde_json::from_str(_instr) {
+		Ok(v) => {d = v;},
+		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
+	}
+
+	match serde_json::from_str(_val) {
+		Ok(v) => {c = v;},
+		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
+	}
+
+	d.insert(String::from(_key),c);
+	retstr = enumerate_json("",&(format!("{}",json!(d).to_string())[..]),0);
+	return retstr;
+}
+
+fn del_json_value(_instr :&str, _key :&str) -> String {
+	let mut d :HashMap<String,Value>;
+	let mut retstr :String = String::from("");
+	match serde_json::from_str(_instr) {
+		Ok(v) => {d = v;},
+		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
+	}
+	if d.contains_key(_key) {
+		d.remove(_key);
+	}
+	retstr = enumerate_json("",&(format!("{}",json!(d).to_string())[..]),0);
+	return retstr;
+}
+
+fn parse_json_value(_instr :&str) {
+	let mut retstr :String;
+	retstr = format!("[{}]",_instr);
+	match serde_json::from_str(_instr) {
+		Err(e) => {
+			eprintln!("{} not valid string [{:?}]", _instr,e);
+			return;
+		},
+		Ok(v) => {
+			match v {
+				Value::Null => {
+					retstr.push_str(&(format!("null")[..]));
+				},
+				Value::Bool(bval) => {
+					retstr.push_str(&(format!("bool({:?})",bval)[..]));
+				},
+				Value::Number(nval) => {
+					retstr.push_str(&(format!("number({})",nval)[..]));
+				},
+				Value::String(sval) => {
+					retstr.push_str(&(format!("string({})",sval)[..]));
+				},
+				Value::Array(aval) => {
+					retstr.push_str(&(format!("array({:?})",aval)[..]));
+				},
+				Value::Object(oval) => {
+					retstr.push_str(&(format!("object({:?})",oval)[..]));
+				},
+			}
+		}
+	}
+	println!("{}", retstr);
+	return;
+}
+
+
+extargs_error_class!{JsonLibError}
+
+fn enumerate_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	for f in sarr.iter() {
+		let s = read_file(f)?;
+		println!("{}", enumerate_json("",&s,0));
+	}
+
+	Ok(())
+}
+
+
+fn add_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	if sarr.len() < 3 {
+		extargs_new_error!{JsonLibError,"need file key value"}
+	}
+
+	let s = read_file(&sarr[0])?;
+	println!("add [{}] value [{}]\n{}",sarr[1],sarr[2],add_json_value(&s,&sarr[1],&sarr[2]));
+	Ok(())
+}
+
+fn del_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	if sarr.len() < 2 {
+		extargs_new_error!{JsonLibError,"need file keys..."}
+	}
+
+	let mut ss = read_file(&sarr[0])?;
+	let mut idx :usize = 1;
+	let mut retstr :String;
+	while idx < sarr.len() {
+		retstr = del_json_value(&ss, &sarr[idx]);
+		println!("delete {}\n{}\n----------------\n{}\n++++++++++++++++++", sarr[idx], ss, retstr);
+		ss = retstr;
+		idx +=1;
+	}
+	Ok(())
+}
+
+fn parse_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	if sarr.len() < 1 {
+		extargs_new_error!{JsonLibError,"need json value"}
+	}
+
+	let mut idx :usize = 0;
+	while idx < sarr.len() {
+		parse_json_value(&sarr[idx]);
+		idx += 1;
+	}
+
+	Ok(())
+}
+
+
+
+#[extargs_map_function(enumerate_handler,add_handler,del_handler,parse_handler)]
+pub fn load_json_cmd(parser :ExtArgsParser) -> Result<(),Box<dyn Error>> {
+	let cmdline = r#"{
+		"enumerate<enumerate_handler>##file ... to enumerate json values##" : {
+			"$" : "+"
+		},
+		"add<add_handler>##file key value to add key value ##" : {
+			"$" : 3
+		},
+		"del<del_handler>##file keys ... to delete key##" : {
+			"$" : "+"
+		},
+		"parse<parse_handler>##str ... to parse values##" : {
+			"$" : "+"
+		}
+	}"#;
+	extargs_load_commandline!(parser,cmdline)?;
+	Ok(())	
+}
+
+
--- test/jsontst/src/main.rs
+++ test/jsontst/src/main.rs
@@ -1,290 +1,56 @@
-use serde_json::{Value,json};
-use std::fs::File;
-use std::io::{BufRead,BufReader,Error};
+#[allow(unused_imports)]
+use extargsparse_codegen::{extargs_load_commandline,ArgSet,extargs_map_function};
+#[allow(unused_imports)]
+use extargsparse_worker::{extargs_error_class,extargs_new_error};
+#[allow(unused_imports)]
+use extargsparse_worker::namespace::{NameSpaceEx};
+#[allow(unused_imports)]
+use extargsparse_worker::options::{ExtArgsOptions};
+#[allow(unused_imports)]
+use extargsparse_worker::argset::{ArgSetImpl};
+use extargsparse_worker::parser::{ExtArgsParser};
+use extargsparse_worker::funccall::{ExtArgsParseFunc};
+#[allow(unused_imports)]
+use extargsparse_worker::const_value::{COMMAND_SET,SUB_COMMAND_JSON_SET,COMMAND_JSON_SET,ENVIRONMENT_SET,ENV_SUB_COMMAND_JSON_SET,ENV_COMMAND_JSON_SET,DEFAULT_SET};
+
+
+#[allow(unused_imports)]
+use std::cell::RefCell;
+#[allow(unused_imports)]
+use std::sync::Arc;
+#[allow(unused_imports)]
+use std::error::Error;
+use std::boxed::Box;
+#[allow(unused_imports)]
+use regex::Regex;
+#[allow(unused_imports)]
+use std::any::Any;
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 
-fn  read_file(path :&str) -> Result<String,Error>  {
-	let fd ;
-	let mut cstr :String = String::from("");
+mod logtrans;
+mod strop;
+mod fileop;
+mod jsonlib;
 
-	match File::open(path) {
-		Ok(file) => fd  = file,
-		Err(err)  => {println!("get error {} {:?}",path, err);return Err(err);}
+
+#[extargs_map_function()]
+fn main() -> Result<(),Box<dyn Error>> {
+	let parser :ExtArgsParser = ExtArgsParser::new(None,None)?;
+	let commandline = r#"
+	{
+		"output|o" : null,
+		"input|i" : null
 	}
-	let reader = BufReader::new(fd);
-	for (_,l) in reader.lines().enumerate() {
-		let l = l.unwrap();
-		cstr.push_str(&(format!("{}\n", l)[..]));
+	"#;
+	extargs_load_commandline!(parser,commandline)?;
+	logtrans::prepare_log(parser.clone())?;
+	jsonlib::load_json_cmd(parser.clone())?;
+	let ores = parser.parse_commandline_ex(None,None,None,None);
+	if ores.is_err() {
+		let e = ores.err().unwrap();
+		eprintln!("{:?}", e);
+		return Err(e);
 	}
-	return Ok(cstr);
-}
-
-fn format_tabs(tabs :i32) -> String {
-	let mut retstr:String = String::from("");
-	for _ in 0..tabs {
-		retstr.push_str(&(format!("    ")[..]));
-	}
-	return retstr;
-}
-
-
-fn array_json(_key :&str, _instr :&str, tabs :i32) -> String {
-	let mut retstr :String = String::from("");
-	let vv :&Vec<Value>;
-	let d :Value;
-	//let mut cnt:usize;
-	match serde_json::from_str(_instr) {
-		Ok(v)  => {
-			d = v;
-			match d.as_array() {
-				Some(dv) => { vv = dv;},
-				None => {eprintln!("---------------\n{}+++++++++++++++++\nnot array",_instr); return retstr;}
-			}
-		}, 
-		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
-	}
-
-	retstr.push_str(&(format_tabs(tabs)[..]));
-	if _key.len() > 0 {
-		retstr.push_str(&(format!("\"{}\" : [",_key)[..]));
-	} else {
-		retstr.push_str("[");
-	}
-
-	for (i,v) in vv.iter().enumerate() {
-		if i > 0 {
-			retstr.push_str(",");
-		}
-		retstr.push_str("\n");
-		if v.is_object() {
-			retstr.push_str(&(enumerate_json("",&(v.to_string()[..]), tabs + 1)[..]));
-			continue;
-		}
-		if v.is_array() {
-			retstr.push_str(&(array_json("",&(v.to_string()[..]),tabs + 1)[..]));
-			continue;
-		}
-
-		retstr.push_str(&(format_tabs(tabs + 1)[..]));
-		retstr.push_str(&(format!("{}",v.to_string())[..]));
-	}
-
-	retstr.push_str("\n");
-	retstr.push_str(&(format_tabs(tabs)[..]));
-	retstr.push_str(&(format!("]")[..]));
-	return retstr;
-}
-
-fn enumerate_json(_key :&str,_instr :&str,tabs :i32) -> String {
-	let d :HashMap<String,Value>;
-	let mut retstr :String = String::from("");
-	let mut i:i32;
-	match serde_json::from_str(_instr) {
-		Ok(v) => {d = v;},
-		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
-	}
-
-	retstr.push_str(&(format_tabs(tabs)[..]));
-	if _key.len() > 0 {
-		retstr.push_str(&(format!("\"{}\" : {{",_key)[..]));		
-	} else {
-		retstr.push_str(&(format!("{{")[..]));
-	}
-
-
-	i = 0;
-	for (s,v) in d {
-		if i > 0 {
-			retstr.push_str(",");
-		}
-		retstr.push_str("\n");
-		if v.is_object() {
-			retstr.push_str(&(enumerate_json(&(s[..]),&(v.to_string()[..]),tabs + 1)[..]));
-			i += 1;
-			continue;
-		}
-
-		if v.is_array() {
-			retstr.push_str(&(array_json(&(s[..]),&(v.to_string()[..]),tabs+1)[..]));
-			i += 1;
-			continue;
-		}
-
-		retstr.push_str(&(format_tabs(tabs+1)[..]));
-		retstr.push_str(&(format!("\"{}\" : {}",s,v)[..]));
-		i += 1;
-	}
-	if i > 0 {
-		retstr.push_str("\n");
-	}
-	retstr.push_str(&(format_tabs(tabs)[..]));
-	retstr.push_str(&(format!("}}")[..]));
-	return retstr;
-}
-
-fn add_json_value(_instr :&str, _key :&str, _val:&str) -> String {
-	let mut d :HashMap<String,Value>;
-	let c :Value;
-	let mut retstr :String = String::from("");
-	match serde_json::from_str(_instr) {
-		Ok(v) => {d = v;},
-		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
-	}
-
-	match serde_json::from_str(_val) {
-		Ok(v) => {c = v;},
-		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
-	}
-
-	d.insert(String::from(_key),c);
-	retstr = enumerate_json("",&(format!("{}",json!(d).to_string())[..]),0);
-	return retstr;
-}
-
-fn del_json_value(_instr :&str, _key :&str) -> String {
-	let mut d :HashMap<String,Value>;
-	let mut retstr :String = String::from("");
-	match serde_json::from_str(_instr) {
-		Ok(v) => {d = v;},
-		Err(e) => {eprintln!("---------------\n{}+++++++++++++++++\nparse error [{:?}]",_instr,e); return retstr;}
-	}
-	if d.contains_key(_key) {
-		d.remove(_key);
-	}
-	retstr = enumerate_json("",&(format!("{}",json!(d).to_string())[..]),0);
-	return retstr;
-}
-
-fn parse_json_value(_instr :&str) {
-	let mut retstr :String;
-	retstr = format!("[{}]",_instr);
-	match serde_json::from_str(_instr) {
-		Err(e) => {
-			eprintln!("{} not valid string [{:?}]", _instr,e);
-			return;
-		},
-		Ok(v) => {
-			match v {
-				Value::Null => {
-					retstr.push_str(&(format!("null")[..]));
-				},
-				Value::Bool(bval) => {
-					retstr.push_str(&(format!("bool({:?})",bval)[..]));
-				},
-				Value::Number(nval) => {
-					retstr.push_str(&(format!("number({})",nval)[..]));
-				},
-				Value::String(sval) => {
-					retstr.push_str(&(format!("string({})",sval)[..]));
-				},
-				Value::Array(aval) => {
-					retstr.push_str(&(format!("array({:?})",aval)[..]));
-				},
-				Value::Object(oval) => {
-					retstr.push_str(&(format!("object({:?})",oval)[..]));
-				},
-			}
-		}
-	}
-	println!("{}", retstr);
-	return;
-}	
-
-
-fn usage(ec :i32,_fmtstr :String) {
-	let mut outstr :String = String::from("");
-	if _fmtstr.len() > 0 {
-		outstr.push_str(&(_fmtstr[..]));
-		outstr.push_str(&(format!("\n")[..]));
-	}
-	outstr.push_str(&(format!("josntst [SUBCOMMANDS]\n[SUBCOMMANDS]\n")[..]));
-	outstr.push_str(&(format!("\tenumerate file...                   to enumerate file\n")[..]));
-	outstr.push_str(&(format!("\tadd file key value                  to add the key value\n")[..]));
-	outstr.push_str(&(format!("\tdel file keys...                    to delete key\n")[..]));
-	outstr.push_str(&(format!("\tparse str...\n")[..]));
-	if ec == 0 {
-		print!("{}",outstr);
-	} else {
-		eprint!("{}",outstr);
-	}
-
-	std::process::exit(ec);
-}
-
-
-fn main() {
-	let argv :Vec<String> = std::env::args().collect();
-	let mut i:usize;
-	let mut ss:String;
-	let mut retstr :String;
-
-	if argv.len() < 2 {
-		usage(3,format!("need at least 2 args"));
-	}
-
-	if argv[1] == "enumerate" {
-		if argv.len() < 3 {
-			usage(3,format!("enumerate need 3 args"));
-		}
-		i = 2;
-		while i < argv.len() {
-			match read_file(&(argv[i][..])) {
-				Ok(s) => {
-					println!("{}",enumerate_json("",&(s[..]),0));
-				},
-				Err(e) => {
-					eprintln!("can not read {} [{:?}]", argv[i],e );
-				}
-			}
-			i += 1;
-		}
-	} else if argv[1] == "add" {
-		if argv.len() < 5 {
-			usage(3,format!("add need 5 args"));
-		}
-		match read_file(&(argv[2][..])) {
-			Ok(s) => {
-				ss = s;
-				println!("add [{}] [{}] into\n{}\n----------------\n{}",
-					argv[3],argv[4],ss, add_json_value(&(ss[..]),&(argv[3][..]),&(argv[4][..])));
-			},
-			Err(e) => {
-				eprintln!("can not read {} [{:?}]", argv[2],e );
-			}
-		}
-	} else if argv[1] == "del" {
-		if argv.len() < 4 {
-			usage(3,format!("del need 4 args"));
-		}
-
-		match read_file(&(argv[2][..])) {
-			Ok(s) => {
-				ss = s;
-				i = 3;
-				while i < argv.len() {
-					retstr = del_json_value(&(ss[..]), &(argv[i][..]));
-					println!("delete {}\n{}\n----------------\n{}\n++++++++++++++++++", argv[i], ss, retstr);
-					ss = retstr;
-					i +=1;
-				}
-			},
-			Err(e) => {
-				eprintln!("can not read {} [{:?}]", argv[2],e );
-			}
-		}
-	} else if argv[1] == "parse" {
-		if argv.len() < 3 {
-			usage(3,format!("parse need 3 args"));
-		}
-
-		i = 2;
-		while i < argv.len() {
-			parse_json_value(&(argv[i][..]));
-			i += 1;
-		}
-	}else {
-		usage(3,format!("not support {}", argv[1]));
-	}
-	return
-	    
+	return Ok(());
 }
