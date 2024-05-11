e72435da4efb9f08b48b6146233cbe9d8c9ff052
jeppeter <jeppeter@gmail.com>
2024-05-11 09:33:25 +0800

    to make proctst ok

--- test/lxtst/src/proctst.rs
+++ test/lxtst/src/proctst.rs
@@ -34,6 +34,53 @@ use crate::*;
 
 extargs_error_class!{ProcHdlError}
 
+struct FdHdlInner {
+	fd :i32,
+}
+
+impl FdHdlInner {
+	fn new(initval :i32) -> Result<Self,Box<dyn Error>> {
+		Ok(Self{
+			fd :initval,
+		})
+	}
+
+	fn set_fd(&mut self, val :i32) -> i32 {
+		let retv :i32 = self.fd;
+		self.fd = val;
+		return retv;
+	}
+
+	fn get_fd(&self) -> i32 {
+		return self.fd;
+	}
+}
+
+#[derive(Clone)]
+struct FdHdl {
+	inner :Arc<UnsafeCell<FdHdlInner>>,
+}
+
+impl FdHdl {
+	fn new(initval :i32) -> Result<Self, Box<dyn Error>> {
+		let retv :Self = Self {
+			inner :Arc::new(UnsafeCell::new(FdHdlInner::new(initval)?)),
+		};
+		Ok(retv)
+	}
+
+	fn set_fd(&mut self, val :i32) -> i32 {
+		let s1 :&mut FdHdlInner = unsafe {&mut *self.inner.get()};
+		return s1.set_fd(val);
+	}
+
+	fn get_fd(&self) -> i32 {
+		let s1 :&mut FdHdlInner = unsafe {&mut *self.inner.get()};
+		return s1.get_fd();
+	}
+
+}
+
 struct ErrorResultInner {
 	res :i32,
 }
@@ -84,12 +131,17 @@ const DEV_NULL :&str = "/dev/null";
 #[allow(unused_assignments)]
 fn daemon_proc(infile:&str ,outfile :&str,errfile :&str, note :&str) -> Result<(),Box<dyn Error>> {
 	debug_trace!("{} daemon",note);
-	let mut infd :i32 = -1;
-	let mut outfd :i32 = -1;
-	let mut errfd :i32 = -1;
+	let mut infd :FdHdl = FdHdl::new(-1)?;
+	let mut outfd :FdHdl = FdHdl::new(-1)?;
+	let mut errfd :FdHdl = FdHdl::new(-1)?;
+	let mut cfd :i32;
+
 	let mut defercall :DeferCall = DeferCall::new();
 	let mut resval :ErrorResult = ErrorResult::new(1)?;
 	let nval = resval.clone();
+	let mut nin = infd.clone();
+	let mut nout = outfd.clone();
+	let mut nerr = errfd.clone();
 	let mut rawinbytes :Vec<u8> = DEV_NULL.as_bytes().to_vec().clone();
 	let mut rawoutbytes :Vec<u8> = DEV_NULL.as_bytes().to_vec().clone();
 	let mut rawerrbytes :Vec<u8> = DEV_NULL.as_bytes().to_vec().clone();
@@ -116,63 +168,72 @@ fn daemon_proc(infile:&str ,outfile :&str,errfile :&str, note :&str) -> Result<(
 
 	defercall.push_call(move || {
 		let getval = nval.get_value();
+		debug_trace!("getval {} infd {} outfd {} errfd {}",getval,nin.get_fd(),nout.get_fd(),nerr.get_fd());
 		if getval != 0 {
-			if infd >= 0 {
+			let mut curfd :i32;
+			curfd = nin.get_fd();
+			if curfd >= 0 {
 				unsafe {
-					libc::close(infd);
+					libc::close(curfd);
 				}
-				infd = -1;
+				nin.set_fd(-1);
 			}
 
-			if outfd >= 0 {
+			curfd = nout.get_fd();
+			if curfd >= 0 {
 				unsafe {
-					libc::close(outfd);
+					libc::close(curfd);
 				}
-				outfd = -1;
+				nout.set_fd(-1);
 			}
 
-			if errfd >= 0 {
+			curfd = nerr.get_fd();
+			if curfd >= 0 {
 				unsafe {
-					libc::close(errfd);
+					libc::close(curfd);
 				}
-				errfd = -1;
+				nerr.set_fd(-1);
 			}
 		}
 	});
 
 	unsafe {
 		let _ptr = rawinbytes.as_ptr() as *const libc::c_char;
-		infd = libc::open(_ptr,libc::O_RDONLY,0);
+		cfd = libc::open(_ptr,libc::O_RDONLY,libc::S_IRWXU | libc::S_IRGRP);
 	}
 
-	if infd < 0 {
+	if cfd < 0 {
 		reti = get_errno!();
 		extargs_new_error!{ProcHdlError,"open [{}] error [{}]",infile,reti}
 	}
+	infd.set_fd(cfd);
 
 	unsafe {
 		let _ptr = rawoutbytes.as_ptr() as *const libc::c_char;
-		outfd = libc::open(_ptr,libc::O_RDWR,0);
+		cfd = libc::open(_ptr,libc::O_RDWR | libc::O_CREAT,libc::S_IRWXU | libc::S_IRGRP);
 	}
 
-	if outfd < 0 {
+	if cfd < 0 {
 		reti = get_errno!();
 		extargs_new_error!{ProcHdlError,"open [{}] error [{}]",outfile,reti}
 	}
 
+	outfd.set_fd(cfd);
+
 
 	unsafe {
 		let _ptr = rawerrbytes.as_ptr() as *const libc::c_char;
-		errfd = libc::open(_ptr,libc::O_RDWR,0);
+		cfd = libc::open(_ptr,libc::O_RDWR | libc::O_CREAT,libc::S_IRWXU | libc::S_IRGRP);
 	}
 
-	if errfd < 0 {
+	if cfd < 0 {
 		reti = get_errno!();
 		extargs_new_error!{ProcHdlError,"open [{}] error [{}]",errfile,reti}
 	}
+	errfd.set_fd(cfd);
 
 	unsafe {
-		reti = libc::dup2(infd,0);
+		reti = libc::dup2(infd.get_fd(),0);
 	}
 	if reti < 0 {
 		extargs_new_error!{ProcHdlError,"can not dup2 [{}] to stdin",infile}
@@ -180,14 +241,14 @@ fn daemon_proc(infile:&str ,outfile :&str,errfile :&str, note :&str) -> Result<(
 
 
 	unsafe {
-		reti = libc::dup2(outfd,1);
+		reti = libc::dup2(outfd.get_fd(),1);
 	}
 	if reti < 0 {
 		extargs_new_error!{ProcHdlError,"can not dup2 [{}] to stdout",outfile}
 	}
 
 	unsafe {
-		reti = libc::dup2(errfd,2);
+		reti = libc::dup2(errfd.get_fd(),2);
 	}
 	if reti < 0 {
 		extargs_new_error!{ProcHdlError,"can not dup2 [{}] to stderr",errfile}
@@ -207,18 +268,16 @@ fn daemon_proc(infile:&str ,outfile :&str,errfile :&str, note :&str) -> Result<(
 	}
 
 	unsafe {
-		libc::close(infd);
-		libc::close(outfd);
-		libc::close(errfd);		
+		libc::close(infd.get_fd());
+		libc::close(outfd.get_fd());
+		libc::close(errfd.get_fd());		
 	}
 
-	infd = -1;
-	outfd = -1;
-	errfd = -1;
+	infd.set_fd(-2);
+	outfd.set_fd(-2);
+	errfd.set_fd(-2);
 
 	/*ok all is ok*/
-
-
 	resval.set_value(0);
 
 	Ok(())
@@ -253,6 +312,7 @@ fn daemonize_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetIm
 		}
 		std::thread::sleep(std::time::Duration::from_millis(500));
 		debug_trace!("cnt [{}]",cnt);
+		println!("cnt [{}]",cnt);
 		cnt += 1;
 	}
 
