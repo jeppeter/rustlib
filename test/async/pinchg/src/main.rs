3d6731be108f9d68380c7db45a017a7bbf9a780d
jeppeter <jeppeter@gmail.com>
2024-05-31 10:50:23 +0800

    to make pinchg

---
+++ test/async/pinchg/src/main.rs
@@ -0,0 +1,50 @@
+use std::pin::Pin;
+use std::marker::PhantomPinned;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+    _marker: PhantomPinned,
+}
+
+
+impl Test {
+    fn new(txt: &str) -> Self {
+        Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+            _marker: PhantomPinned, // This makes our type `!Unpin`
+        }
+    }
+
+    fn init(self: Pin<&mut Self>) {
+        let self_ptr: *const String = &self.a;
+        let this = unsafe { self.get_unchecked_mut() };
+        this.b = self_ptr;
+    }
+
+    #[allow(dead_code)]
+    fn a(self: Pin<&Self>) -> &str {
+        &self.get_ref().a
+    }
+
+    #[allow(dead_code)]
+    fn b(self: Pin<&Self>) -> &String {
+        assert!(!self.b.is_null(), "Test::b called without Test::init being called first");
+        unsafe { &*(self.b) }
+    }
+}
+
+fn main() {
+   let mut test1 = Test::new("test1");
+   let mut test1_pin = unsafe { Pin::new_unchecked(&mut test1) };
+   Test::init(test1_pin.as_mut());
+
+   drop(test1_pin);
+   println!(r#"test1.b points to "test1": {:?}..."#, test1.b);
+
+   let mut test2 = Test::new("test2");
+   std::mem::swap(&mut test1, &mut test2);
+   println!("... and now it points nowhere: {:?}", test1.b);
+}
