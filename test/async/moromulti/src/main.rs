950280d4beffa3448be1f8dbce00544f10300b01
jeppeter <jeppeter@gmail.com>
2024-09-25 13:10:08 +0800

    to make structured concurrency with one thread

--- test/async/moromulti/src/main.rs
+++ test/async/moromulti/src/main.rs
@@ -5,19 +5,21 @@ use std::sync::RwLock;
 pub async fn main() {
     let value = RwLock::new(22);
     let result2 = *value.read().unwrap();
-    println!("{result2:?}"); // prints 88
+    println!("threadid {:?} {result2:?}",  std::thread::current().id());
     moro::async_scope!(|scope| {
         scope.spawn(async {
 	        // we can spawn nested tasks
             scope.spawn(async {
 	            // and access values that outlive the scope
                 *value.write().unwrap() *= 3; 
+                println!("threadid {:?} *3",  std::thread::current().id());
             });
 
             *value.write().unwrap() += 3;
+            println!("threadid {:?} +3",  std::thread::current().id());
         });
     })
     .await;
     let result = *value.read().unwrap();
-    println!("{result:?}"); // prints 69 for it will give 
+    println!("threadid {:?} {result:?}",  std::thread::current().id());
 }
