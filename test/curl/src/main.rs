73dbb2b236e09fe286e6509fb3c81c103f92d00a
jeppeter <jeppeter@gmail.com>
2025-03-22 19:27:53 +0800

    to add curl

---
+++ test/curl/src/main.rs
@@ -0,0 +1,55 @@
+#[allow(unused_imports)]
+use extargsparse_codegen::{extargs_load_commandline,ArgSet,extargs_map_function};
+#[allow(unused_imports)]
+use extargsparse_worker::{extargs_error_class,extargs_new_error};
+#[allow(unused_imports)]
+use extargsparse_worker::namespace::{NameSpaceEx};
+#[allow(unused_imports)]
+use extargsparse_worker::options::{ExtArgsOptions};
+#[allow(unused_imports)]
+use extargsparse_worker::argset::{ArgSetImpl};
+use extargsparse_worker::parser::{ExtArgsParser};
+use extargsparse_worker::funccall::{ExtArgsParseFunc};
+#[allow(unused_imports)]
+use extargsparse_worker::const_value::{COMMAND_SET,SUB_COMMAND_JSON_SET,COMMAND_JSON_SET,ENVIRONMENT_SET,ENV_SUB_COMMAND_JSON_SET,ENV_COMMAND_JSON_SET,DEFAULT_SET};
+
+
+#[allow(unused_imports)]
+use std::cell::RefCell;
+#[allow(unused_imports)]
+use std::sync::Arc;
+#[allow(unused_imports)]
+use std::error::Error;
+use std::boxed::Box;
+#[allow(unused_imports)]
+use regex::Regex;
+#[allow(unused_imports)]
+use std::any::Any;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+fn curl_handler(ns :NameSpaceEx) -> Result<(),Box<dyn Error>> {
+	Ok(())
+}
+
+#[extargs_map_function()]
+fn main() -> Result<(),Box<dyn Error>> {
+	let parser :ExtArgsParser = ExtArgsParser::new(None,None)?;
+	let commandline = format!(r#"
+	{{
+		"header|H##header for user extend##" : [],
+		"method|X##method support put get post##" : "GET",
+		"$" : "+"
+	}}
+	"#);
+	extargs_load_commandline!(parser,&commandline)?;
+	let ores = parser.parse_commandline_ex(None,None,None,None);
+	if ores.is_err() {
+		let e = ores.err().unwrap();
+		eprintln!("{:?}", e);
+		return Err(e);
+	}
+
+	let ns = ores.unwrap();
+	return curl_handler(ns);
+}
